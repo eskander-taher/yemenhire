757c04a31460208a8b04272be1ba5c2cfacc74cd
eskander-taher <eskander.alshaibany@gmail.com>
2025-07-19 16:26:11 +0300

    updat new server url with ssl

--- src/lib/myAxios.ts
+++ src/lib/myAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
 	process.env.NODE_ENV === "production"
-		? "http://185.198.152.113:4000/api"
+		? "https://api.yemenhire.com/api"
 		: "http://localhost:4000/api";
 
 const myAxios = axios.create({ baseURL });
